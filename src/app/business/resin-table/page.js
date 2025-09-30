7b195f08c71e831a95bd7b953a602f005ca0ea44
eitanakagawa <eitaroid@gmail.com>
2025-09-30 14:08:02 +0900

    レジンテーブル事業 ギャラリー追加

--- src/app/business/resin-table/page.js
+++ src/app/business/resin-table/page.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import ResinTableStrengths from "./components/ResinTableStrengths";
+import Gallery from "./components/Gallery";
 
 export default function ResinTable() {
     return (
         <main>
             <Hero />
             <ResinTableStrengths />
+            <Gallery />
         </main>
     );
 }

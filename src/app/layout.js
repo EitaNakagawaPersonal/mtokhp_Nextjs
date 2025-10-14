c3a4df9c0f7ae4766978af2425704e779eb2f919
EitaNakagawaPersonal <nkgweita1023@gmail.com>
2025-10-14 17:44:20 +0900

    GoogleAnalyticsを作成

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono, Noto_Serif_JP, Inter } from "next/font/google";
 import "./globals.css";
 import InstagramButton from "@/components/InstagramButton";
 import Header from "@/components/Header";
+import { GoogleAnalytics } from './components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${notoSerif.variable} antialiased`}
       >
+        {process.env.NEXT_PUBLIC_GA_ID && <GoogleAnalytics />}
         <Header />
         <main className="pt-20">{children}</main>
         <InstagramButton />
